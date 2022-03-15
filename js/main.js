16469828378a835fb914fc3486167f913d6a007a
Camille Estanguet <estanguetcamille@gmail.com>
2022-03-15 12:13:54 +0100

    Changing texture

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@
         new THREE.SphereGeometry(5, 50, 50),
         new THREE.MeshBasicMaterial({
 			//color: 0x77DD33
-            map: new THREE.TextureLoader().load('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/570c9426-37ef-4869-b888-7da245b8a19f/del9sm5-8028b7e2-25ff-436f-889c-cb073876557a.jpg/v1/fill/w_1280,h_640,q_75,strp/earth_texture_map_by_wdawdawdwdaw_del9sm5-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjQwIiwicGF0aCI6IlwvZlwvNTcwYzk0MjYtMzdlZi00ODY5LWI4ODgtN2RhMjQ1YjhhMTlmXC9kZWw5c201LTgwMjhiN2UyLTI1ZmYtNDM2Zi04ODljLWNiMDczODc2NTU3YS5qcGciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.mopKGVfWzfNfkf3m1GqexwCAAHsJX4-Rp9cdpmcAahk')
+            map: new THREE.TextureLoader().load('http://lakartxela.iutbayonne.univ-pau.fr/~cestangue001/Wartpped-1/media/texxtur.png')
         })
 	)
 	sphere.rotation.y = -Math.PI / 2
@@ -62,14 +62,14 @@
 
 	//Group of objects creation
 	const group = new THREE.Group()
-	const groupEarth = new THREE.Group()
+	const points = new THREE.Group()
 
 	//Create point
 	function createPoint(lat, long){
 		const point = new THREE.Mesh(
 			new THREE.BoxGeometry(0.1, 0.1, 0.4),
 			new THREE.MeshBasicMaterial({
-				color: '#ffff00'
+				color: '#ff5500'
 			})
 		)
 	
@@ -87,7 +87,7 @@
 
 	point.geometry.applyMatrix4( new THREE.Matrix4().makeTranslation(0, 0, -0.2))
 
-	groupEarth.add(point)
+	points.add(point)
 	}
 
 	//Creating the points needed
@@ -97,8 +97,8 @@
 	createPoint(52.2333, 9.2)
 
 	//Adding The Earth and the Points to the group and the scene
-    groupEarth.add(sphere)
-	group.add(groupEarth)
+    group.add(sphere)
+	group.add(points)
     scene.add(group)
 
 ///======================================================================ANIMATION=============================================================///
@@ -107,15 +107,14 @@
 function animate() {
 	requestAnimationFrame(animate);
 	renderer.render(scene, camera);
-    //sphere.rotation.y += 0.008
-	groupEarth.rotation.y += 0.001
-    //group.rotation.y = mouse.x * 0.5
+    group.rotation.y += 0.001
 	raycaster.setFromCamera( pointer, camera )
-	const intersects = raycaster.intersectObjects( groupEarth.children )
+	const intersects = raycaster.intersectObjects( scene.children )
+	//console.log(intersects)
 
 	for ( let i = 0; i < intersects.length; i ++ ) {
 
-		//intersects[ i ].object.material.color.set( 0xff0000 )
+		//intersects[i].object.material.color.set(0xff0000)
 
 	}
 }
