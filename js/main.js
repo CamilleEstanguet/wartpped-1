f6940ce49853bc6939b081cdb81aadbd255c3733
Kevin Levron <kevin.levron@gmail.com>
2022-03-16 14:16:16 +0100

    move glsl

--- js/main.js
+++ js/main.js
@@ -1,9 +1,51 @@
-import vertexShader from '../shaders/vertex.glsl'
-import fragmentShader from '../shaders/fragment.glsl'
-import atmosphereVertexShader from '../shaders/atmosphereVertex.glsl'
-import atmosphereFragmentShader from '../shaders/atmosphereFragment.glsl'
+// import vertexShader from '../shaders/vertex.glsl'
+// import fragmentShader from '../shaders/fragment.glsl'
+// import atmosphereVertexShader from '../shaders/atmosphereVertex.glsl'
+// import atmosphereFragmentShader from '../shaders/atmosphereFragment.glsl'
 
 let camera, scene, renderer;
+
+const vertexShader = `
+  varying vec2 vertexUV;
+  varying vec3 vertexNormal;
+
+  void main() {
+    vertexUV = uv;
+    vertexNormal = normalize(normalMatrix * normal);
+    
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0);
+  }
+`
+const fragmentShader = `
+  uniform sampler2D globeTexture;
+
+  varying vec2 vertexUV; // [0, 0.24]
+  varying vec3 vertexNormal;
+
+  void main() {
+    float intensity = 1.05 - dot(vertexNormal, vec3(0.0, 0.0, 1.0 ));
+    vec3 atmosphere = vec3(0.3, 0.6, 1.0) * pow(intensity, 1.5);
+    
+    gl_FragColor = vec4(atmosphere + texture2D(globeTexture, vertexUV).xyz, 1.0); 
+  }
+`
+const atmosphereVertexShader = `
+  varying vec3 vertexNormal;
+
+  void main() {
+    vertexNormal = normal;
+    
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0);
+  }
+`
+const atmosphereFragmentShader = `
+  varying vec3 vertexNormal;
+
+  void main() {
+    float intensity = pow(0.2 - dot(vertexNormal, vec3(0.0, 0.0, 0.5 )), 1.0);
+    gl_FragColor = vec4(0.3, 0.6, 1.0, 1.0) * intensity; 
+  }
+`
 
 const canvasContainer = document.querySelector('#canvasContainer')
 
@@ -51,7 +93,7 @@ const mouse = {
         fragmentShader,
         uniforms: {
           globeTexture: {
-            value: new THREE.TextureLoader().load('./textures/texxtur.png')
+            value: new THREE.TextureLoader().load('./media/texxtur.png')
           }
         }
       })
